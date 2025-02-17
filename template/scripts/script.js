5cb68aa0d3e075dc344c6da5e9733d5dd119d95a
Isabelle Apelgren <isabelleapelgren@gmail.com>
2025-02-17 17:19:01 +0100

    trailer caroussel done

--- template/scripts/script.js
+++ template/scripts/script.js
@@ -3,6 +3,7 @@ import oData from './data/data.js';
 import { createCard } from './components/movieCard.js'; 
 import { getElement } from './utils/domUtils.js';
 import { displaymovieDetails } from './modules/eventHandlers.js'
+import { renderTrailers } from './modules/caroussel.js';
 
 if(window.location.pathname === '/template/' || window.location.pathname === '/template/index.html') {
     console.log('index.html');
@@ -22,32 +23,49 @@ if(window.location.pathname === '/template/' || window.location.pathname === '/t
 }
 
 async function setupMain(){
+    await fetchTopMovies();
     setupRecs();
+    setupTrailers();
 }
 
 async function setupFavorites(){
 
 }
 
-async function setupMovie(){
-
+async function setupMovie(movie){
+    console.log('hello');
+    
 }
 
 async function setupSearch(){
 
 }
 
-async function setupRecs(){
-    await fetchTopMovies();
-    
+function setupRecs(){
     const recsRef = getElement('#cardContainer');
 
     oData.topMovieList.sort(() => Math.random() -0.5);
 
-    for(let movie of oData.topMovieList){   
+    for(let movie of oData.topMovieList){
+        console.log(movie);
         let card = createCard(movie);
         recsRef.appendChild(card);
+        card.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.location.href = './movie.html';
+        });
     }
+}
+
+function setupTrailers(){
+    oData.topMovieList.sort(() => Math.random() -0.5);
+
+    let fiveTrailers = oData.topMovieList.slice(0, 5);
+    console.log(fiveTrailers);
+    for (let i = 0; i < fiveTrailers.length; i++) {
+        renderTrailers(fiveTrailers[i], i+1)
+    }
+    
 }
 
     
